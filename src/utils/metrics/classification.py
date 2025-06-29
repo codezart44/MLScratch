68e12b6debeb0fe1874b7193b9db8ee89908fdbf
oskarew <oswa.public@gmail.com>
2025-06-29 11:46:04 +0100

    implemented binary cross entropy (bce) for logisitic regression

--- src/utils/metrics/classification.py
+++ src/utils/metrics/classification.py
@@ -40,5 +40,35 @@ def accuracy_score(
     >>> accuracy_score(y_true, y_pred)
     0.0
     """
-    accuracy = (y_true == y_pred).sum() / y_true.shape[0]
+    accuracy = np.mean(y_true == y_pred)
     return accuracy
+
+def bce_score(
+        y_true : np.ndarray,
+        y_pred : np.ndarray
+        ) -> float:
+    """
+    Compute the binary cross entropy score of the predicted labels against the true labels.
+
+    >>> # The mean negative log likelihood, abbreviated NLL also
+    >>> bce = 1/N * sum(y * log(h) + (1-y) * log(1-h))
+
+    Parameters
+    ----------
+    y_true : ndarray
+        An array of the true class labels.
+    y_pred : ndarray
+        An array of the predicted class labels. Must be of the same length as `y_true`.  
+
+    Returns
+    -------
+    float
+        The binary cross entropy score of the predicted class labels.
+
+    Examples
+    --------
+    >>> ... to be continued
+    """
+    y_pred = np.clip(y_pred, 1e-15, 1 - 1e-15)  # avoid underflow in log expressions
+    bce = -np.mean(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
+    return bce
