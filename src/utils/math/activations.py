380cf4ece890e3d1c9dba6ea3f0e4cc42d49c49d
oskarew <oswa.public@gmail.com>
2025-06-29 11:45:17 +0100

    added linear activation for linear regression

--- src/utils/math/activations.py
+++ src/utils/math/activations.py
@@ -1,5 +1,18 @@
 import numpy as np
 
+linear_f = lambda z: z
+sigmoid_f = lambda z: 1 / (1 + np.exp(-z))
+tanh_f = lambda z: np.tanh(z)
+threshold_f = lambda z: np.greater_equal(z, 0).astype(int)
+relu_f = lambda z: np.max(z, 0)
+
+def linear(z: np.ndarray) -> np.ndarray:
+    """
+    >>> Identity function
+    >>> linear(x) = x
+    """
+    return z
+
 def sigmoid(z: np.ndarray) -> np.ndarray:  # aka logistic function
     """
     >>> sigmoid(z) = 1 / (1 + exp(-z))
